a7bb32dc02c8300d9519c39a37df3e7419230e15
Armand Philippot <git@armandphilippot.com>
2022-07-30 22:03:35 +0200

    test: add tests for recursive option

--- tests/walk-dir.spec.ts
+++ tests/walk-dir.spec.ts
@@ -3,15 +3,13 @@ import { Dirent } from 'fs';
 import { readdir } from 'fs/promises';
 import { resolve } from 'path';
 import { walkDir } from '../dist/index.js';
-import { FileType } from '../dist/ts/enums.js';
-import { Directory, RegularFile } from '../dist/ts/types.js';
+import { getFilesIn, getSubdirectoriesIn } from '../dist/utils/helpers.js';
 import { getDirname } from '../dist/utils/paths.js';
 
 const FIXTURES_PATH = resolve(getDirname(import.meta), './fixtures/');
 
 const test = anyTest as TestFn<{
   fixtures: Dirent[];
-  root: (RegularFile | Directory)[];
 }>;
 
 test.beforeEach(async (t) => {
@@ -20,11 +18,10 @@ test.beforeEach(async (t) => {
       encoding: 'utf8',
       withFileTypes: true,
     }),
-    root: await walkDir(FIXTURES_PATH),
   };
 });
 
-test('returns the correct number of files & directories in root folder', (t) => {
+test('returns the correct number of files & directories in root folder', async (t) => {
   const fixturesCount = t.context.fixtures.length;
   const filesFixturesCount = t.context.fixtures.filter((fileOrDir) =>
     fileOrDir.isFile()
@@ -32,14 +29,31 @@ test('returns the correct number of files & directories in root folder', (t) => 
   const directoriesFixturesCount = t.context.fixtures.filter((fileOrDir) =>
     fileOrDir.isDirectory()
   ).length;
-  const rootDirectories = t.context.root.filter(
-    (fileOrDir) => fileOrDir.type === FileType.DIRECTORY
-  ) as Directory[];
-  const rootFiles = t.context.root.filter(
-    (fileOrDir) => fileOrDir.type === FileType.FILE
-  ) as RegularFile[];
+  const root = await walkDir(FIXTURES_PATH);
+  const rootDirectories = getSubdirectoriesIn(root);
+  const rootFiles = getFilesIn(root);
 
-  t.is(t.context.root.length, fixturesCount);
+  t.is(root.length, fixturesCount);
   t.is(rootDirectories.length, directoriesFixturesCount);
   t.is(rootFiles.length, filesFixturesCount);
+});
+
+test('returns root subdirectories content if recursive', async (t) => {
+  const root = await walkDir(FIXTURES_PATH, { recursive: true });
+  const rootDirectories = getSubdirectoriesIn(root);
+
+  rootDirectories.forEach((subDir) => {
+    t.truthy(subDir.files);
+    t.truthy(subDir.subdirectories);
+  });
+});
+
+test('does not return root subdirectories content if not recursive', async (t) => {
+  const root = await walkDir(FIXTURES_PATH, { recursive: false });
+  const rootDirectories = getSubdirectoriesIn(root);
+
+  rootDirectories.forEach((subDir) => {
+    t.falsy(subDir.files);
+    t.falsy(subDir.subdirectories);
+  });
 });
